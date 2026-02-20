0e11edeacfd6b7d9122bf880d3f428da57372592
Sujan Basnet <basnetsujan1828420@gmail.com>
2026-02-20 14:53:43 +0545

    ten: scrape book with inset drop shadow

--- src/components/ScrapeBook.jsx
+++ src/components/ScrapeBook.jsx
@@ -13,17 +13,34 @@ const Wrapper = styled.article`
   background: peachpuff;
   padding: 64px;
   width: 90%;
+  max-width: 500px;
+  margin: 0 auto;
+  box-shadow: inset 1px 2px 4px hsl(0deg 0% 0% / 0.3);
+  border-radius: 1px;
 `;
 
 const Card = styled.div`
   padding: 16px;
   background: white;
+  box-shadow: 1px 2px 4px hsl(0deg 0% 0% / 0.3);
+  position: relative;
+  border-radius: 1px;
 `;
 
-const Photo = styled.img``;
+const Photo = styled.img`
+  width: 100%;
+  display: block;
+  border-radius: 1px;
+`;
 
 const Sticker = styled.div`
   font-size: 5rem;
+  position: absolute;
+  top: 0;
+  left: 0;
+  line-height: 5rem;
+  transform: translate(-40%, -40%);
+  filter: drop-shadow(1px 2px 6px hsl(0deg 0% 0% / 0.5));
 `;
 
 export default function SimpleCard() {
