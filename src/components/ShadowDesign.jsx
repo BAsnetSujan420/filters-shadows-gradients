5b14f233f0165a25a5b31964b665b10ded8701de
Sujan Basnet <basnetsujan1828420@gmail.com>
2026-02-23 14:07:12 +0545

    twelve: layered color-matched shadows

--- src/components/ShadowDesign.jsx
+++ src/components/ShadowDesign.jsx
@@ -29,13 +29,21 @@ const FirstBox = styled(Box)`
 const SecondBox = styled(Box)`
   width: 100px;
   height: 100px;
-  box-shadow: 0.5px 1px 1px hsl(220deg 60% 50% / 0.7);
+  box-shadow:
+    1px 2px 2px hsl(220deg 60% 50% / 0.333),
+    2px 4px 4px hsl(220deg 60% 50% / 0.333),
+    3px 6px 6px hsl(220deg 60% 50% / 0.333);
 `;
 
 const ThirdBox = styled(Box)`
   width: 150px;
   height: 150px;
-  box-shadow: 0.5px 1px 1px hsl(220deg 60% 50% / 0.7);
+  box-shadow:
+    1px 2px 2px hsl(220deg 60% 50% / 0.2),
+    2px 4px 4px hsl(220deg 60% 50% / 0.2),
+    4px 8px 8px hsl(220deg 60% 50% / 0.2),
+    8px 16px 16px hsl(220deg 60% 50% / 0.2),
+    16px 32px 32px hsl(220deg 60% 50% / 0.2);
 `;
 
 export default function ShadowDesign() {
